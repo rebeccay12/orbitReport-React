bb1132b4f1af43e47714f7cc874e052a9a5c316a
Rebecca Yeager <rebeccayeager@Rebeccas-Air.lan>
2023-08-11 22:08:02 -0500

    Friday night work on task 4 and 5

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+
+function Banner () {
+    let orbitReportInstructions = 
+    <header>
+        Orbit Report
+            <p>
+                Click on the buttons to see the satellites in that orbit type.
+            </p>
+    </header>    
+    
+    return orbitReportInstructions
+};
+
+export default Banner
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,26 @@
-const Buttons = () => {
+import satData from "./satData.js"
+
+const Buttons = (props) => {
+  const { filterByType, setSat, displayStats } = props;
+  
   return (
     <div>
+      <button onClick={() => filterByType('')}> Filter xxx Sats</button>
+      <button onClick={() => displayStats()}>Display Sats</button>
+      
+      {/* {displaySats.map((sat, id) => {
+  return (
+    <button onClick={() => filterByType(sat)} key={id}>
+      {sat} Orbit
+    </button>
+  );
+})} */}
+//code continues
       <button>Placeholder Button</button>
       <button>All Orbits</button>
       </div>
   );
+  <button onClick={() => setSat()}>Set Sat</button>
 };
 
 export default Buttons;
